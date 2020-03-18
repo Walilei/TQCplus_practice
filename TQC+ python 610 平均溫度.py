fa16efdb292cf0e9b6f26087c64e37dce1cae9cb
Walilei <walilei@gmail.com>
2020-03-19 03:28:00 +0800

    Update TQC+ python 610 平均溫度.py

--- TQC+ python 610 平均溫度.py
+++ TQC+ python 610 平均溫度.py
@@ -4,15 +4,12 @@
 提示1：平均溫度輸出到小數點後第二位。
 提示2：最高溫度及最低溫度的輸出，如為31時，則輸出31，如為31.1時，則輸出31.1。
 '''
-
 lst = []
 
 for i in range(4):
     print(f'Week {i+1}:')
     for j in range(3):
-        print(f'Day {j+1}:', end='')
-        user = eval(input())
-        print(user)
+        user = eval(input(f'Day {j+1}:'))
         lst.append(user)
 
 print(f'Average: {sum(lst)/len(lst):.2f}')
